e8e8f51f1f85a8c6a54346e944de4fa6d5f3527b
orlando <glee1470@naver.com>
2023-11-07 08:58:41 +0900

    슬라이드 버튼 수정

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function moveleft() {
+function moveleft() {    
     document.querySelector('#firstslide').addEventListener('click', function(){
+        const secondslide = document.querySelector('#secondslide');
+        secondslide.style.backgroundColor = 'white';
+        secondslide.style.color = 'black';
+
+        const firstslide = document.querySelector('#firstslide');
+        firstslide.style.backgroundColor = 'black';
+        firstslide.style.color = 'white';
         document.querySelector('.container').style.transform = 'translate(0px)';
     })
 }
 
 function moveright() {
     document.querySelector('#secondslide').addEventListener('click', function(){
+        const firstslide = document.querySelector('#firstslide');
+        firstslide.style.backgroundColor = 'white';
+        firstslide.style.color = 'black';
+
+        const secondslide = document.querySelector('#secondslide');
+        secondslide.style.backgroundColor = 'black';
+        secondslide.style.color = 'white';
         document.querySelector('.container').style.transform = 'translate(-1170px)';
     })
 }
+
